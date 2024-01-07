144805a7858101a34b64ee18c05d3258e2aa6fa9
CyrilAshong <ashongcyril2704@gmail.com>
2024-01-07 03:15:21 +0000

    modified index.js file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const marketRoutes = require('./routes/markets');
 const shoeRoutes = require('./routes/shoes');
 const clothesRouter = require('./routes/clothes');
 const electricalsRouter = require('./routes/electricals');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = 3003;
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(groceryRoutes);
 app.use("/markets" ,marketRoutes);
